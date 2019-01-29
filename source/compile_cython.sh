64626f6a3e79c183356ecf4d0f57d9767ab77e21
Calvin Sykes <calvin.v.sykes@durham.ac.uk>
2019-01-29 12:34:49 +0000

    fix cython build script to work with icc or gcc

--- source/compile_cython.sh
+++ source/compile_cython.sh
@@ -1,1 +1,5 @@
-python cython_fns_setup.py build_ext --inplace
+# For GNU compiler
+#python cython_fns_setup.py build_ext --inplace 
+
+# For Intel compiler
+CC=icc LINKCC=icc LDSHARED="icc -shared" python cython_fns_setup.py build_ext --inplace
