e4de63023554c64e68d28e924f364d9fc953d41c
Calvin Sykes <calvin.v.sykes@durham.ac.uk>
2019-01-29 12:33:03 +0000

    remove redundant code

--- source/colioniz.py
+++ source/colioniz.py
@@ -18,18 +18,6 @@ def get_rate(Te, dE, P, A, X, K):
     rateval[w] = 0.0
     return rateval
 
-def rate_function(Te, dE, P, A, X, K, tmin, tmax):
-#	tmax *= 1000.0 # Convert keV to eV
-    rateval = get_rate(Te, dE, P, A, X, K)
-    wn = np.where(rateval<0.0)
-#	wn = np.where((rateval<0.0)|(Te<dE))
-    rateval[wn] = 0.0
-#	wx = np.where(Te>tmax)
-#	if wn[0].size > 0:
-#		rateval[wn] = 0.0#get_rate(tmin, dE, P, A, X, K)
-#	if wx[0].size > 0:
-#		rateval[wx] = get_rate(tmax, dE, P, A, X, K)
-    return rateval
 
 def rate_function_arr(Te, arr):
     dE = arr[0]
@@ -51,16 +39,20 @@ def rate_function_arr(Te, arr):
 #		rateval[wx] = get_rate(tmax, dE, P, A, X, K)
     return rateval
 
+
 def rate_function_Dere2007(Te, arr):
     """
     temperature is in eV
     """
-    tck = interpolate.splrep(arr[2], arr[3], s=1)
-    redt = Te/arr[0] # ratio of temperature in eV to IP 
-    xval = 1.0 - np.log(2.0)/np.log(2.0 + redt)
-    rho = interpolate.splev(xval, tck, der=0, ext=1)
+    tck = interpolate.splrep(arr[2], arr[3], s=1) # spline representation of ionisation rate
+    # arr[2] is x values, arr[3] is scaled rates
+    redt = Te/arr[0] # ratio of temperature in eV to IP
+    xval = 1.0 - np.log(2.0)/np.log(2.0 + redt) # Eq. (4) of Dere+ 2007
+    #print(xval)
+    rho = interpolate.splev(xval, tck, der=0, ext=0)
     rateval = (redt**-0.5) * (arr[0]**-1.5) * (1.0E-6*rho) * specialfunc.exp1(1.0/redt)
     return rateval
+
 
 def rate_function_Chianti(Te, arr, arrTe):
     """
@@ -68,38 +60,6 @@ def rate_function_Chianti(Te, arr, arrTe):
     """
     return np.interp(Te, arrTe, arr)
 
-def rate(ion,electemp,elidx):
-    #print "Have you checked that the electron temperature is in eV?"
-    #assert(False)
-    if ion == "H I":
-        dE, P, A, X, K, tmin, tmax = 13.59843, 0.0, 0.291E-07, 0.2320, 0.39, 1, 20
-    elif ion == "D I":
-        dE, P, A, X, K, tmin, tmax = 13.60213, 0.0, 0.291E-07, 0.2320, 0.39, 1, 20
-    elif ion == "He I":
-        dE, P, A, X, K, tmin, tmax = 24.58741, 0.0, 0.175E-07, 0.1800, 0.35, 1, 20
-    elif ion == "He II":
-        dE, P, A, X, K, tmin, tmax = 54.41778, 1.0, 0.205E-08, 0.2650, 0.25, 3, 20
-    elif ion == "N I":
-        dE, P, A, X, K, tmin, tmax = 14.53414, 0.0, 0.482E-07, 0.0652, 0.42, 1, 20
-    elif ion == "N II":
-        dE, P, A, X, K, tmin, tmax = 29.60130, 0.0, 0.298E-07, 0.3100, 0.30, 2, 20
-    elif ion == "N III":
-        dE, P, A, X, K, tmin, tmax = 47.44924, 1.0, 0.810E-08, 0.3500, 0.24, 2, 20
-    elif ion == "N IV":
-        dE, P, A, X, K, tmin, tmax = 77.47350, 1.0, 0.371E-08, 0.5490, 0.18, 4, 20
-    elif ion == "Si I":
-        dE, P, A, X, K, tmin, tmax = 8.151690, 1.0, 0.188E-06, 0.3760, 0.25, 1, 20
-    elif ion == "Si II":
-        dE, P, A, X, K, tmin, tmax = 16.34585, 1.0, 0.643E-07, 0.6320, 0.20, 1, 20
-    elif ion == "Si III":
-        dE, P, A, X, K, tmin, tmax = 33.49302, 1.0, 0.201E-07, 0.4730, 0.22, 2, 20
-    elif ion == "Si IV":
-        dE, P, A, X, K, tmin, tmax = 45.14181, 1.0, 0.494E-08, 0.1720, 0.23, 3, 20
-    else:
-        return np.zeros(electemp.size)
-    # Use the ionization energy from NIST:
-    dE = elidx[ion][2]
-    return rate_function(electemp, dE, P, A, X, K, tmin, tmax)
 
 def load_data(elidx, rates="Voronov1997"):
     """
@@ -126,7 +86,7 @@ def load_data(elidx, rates="Voronov1997"):
         lines = open("data/colioniz_dere2007.dat").readlines()
         for i in range(len(lines)):
             if len(lines[i].strip())==0: continue # Nothing on a line
-            if lines[i].strip()[0]=='#': continue  # A comment line
+            if lines[i].strip()[0]=='#': continue # A comment line
             linspl = lines[i].split("|")
             elem = misc.numtoelem(int(linspl[0]))
             ionstage = misc.numtorn(int(linspl[1]),subone=False)
--- source/radfields.py
+++ source/radfields.py
@@ -5,48 +5,20 @@ import constants
 import logger
 import os
 
-def HMbackground_z0_sternberg(nu=None,maxvu=200.0,num=10000):
-    Jnu0 = 2.0E-23
-    nu0 = 3.287E15
-    if nu is None:
-        nu = np.linspace(5.0E-2,maxvu,num)
-        J = Jnu0*np.ones(num)
-    else:
-        nu /= nu0
-        J = Jnu0*np.ones(nu.size)
-    w = np.where(nu>4.0)
-    J[w] *= 2.512E-2 * nu[w]**-0.46
-    w = np.where((nu>=1.0) & (nu<=4.0))
-    J[w] *= nu[w]**-3.13
-    w = np.where((nu>=0.3) & (nu<1.0))
-    J[w] *= nu[w]**-5.41
-    w = np.where(nu<0.3)
-    J[w] *= 1.051E2 * nu[w]**-1.5
-    return J, nu*nu0
-
-def HMbackground(elID,redshift=3.0, version='12', alpha_UV=0):
-    Jnu, nu, discretised_z = _HM_background_impl(elID, redshift, version, alpha_UV)
-    logger.log("info", "Using HM{1:s} background at z={0:f}".format(discretised_z, version))
-    return Jnu, nu
-
-def _HM_background_impl(elID,redshift, version, alpha_UV):
-    const = constants.get()
-    planck  = const["planck"]
-    elvolt  = const["elvolt"]
+def HMredshifts(version='12'):
+    """return list of redshifts H&M field is tabulated at"""
     if version in {'12', '15'}:
         usecols = tuple(range(60))
     elif version == '05':
         usecols = tuple(range(50))
     data = np.loadtxt(os.path.join(os.path.dirname(__file__), "data/radfields/HM{:s}_UVB.dat").format(version), usecols=usecols)
-    rdshlist = data[0,:]
-    amin = np.argmin(np.abs(rdshlist-redshift))
-    waveAt, Jnut = data[1:,0], data[1:,amin+1]
-    waveA = waveAt[1:]*1.0E-10
-    #w = np.where(waveAt < 912.0)
-    Jnu = Jnut[1:]
-    nu = 299792458.0/waveA
-    # Interpolate the Haardt & Madau background around each of the ionization potentials
-    #ediff = 0.00001
+    return data[0,:]    
+
+def extra_interp(elID, nu, Jnu):
+    """add finer interpolation to radiation field around ionisation potentials"""
+    const = constants.get()
+    planck  = const["planck"]
+    elvolt  = const["elvolt"]
     ediff = 1.0E-10
     ekeys = elID.keys()
     Jnurev = Jnu[::-1]
@@ -57,7 +29,7 @@ def _HM_background_impl(elID,redshift, version, alpha_UV):
         nup = (elID[ekeys[i]].ip+ediff)*elvolt/planck
         num = (elID[ekeys[i]].ip-ediff)*elvolt/planck
         Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
+        Jnuvm = np.interp(num,nurev,Jnurev)
         nuadd[2*i]    = nup
         nuadd[2*i+1]  = num
         Jnuadd[2*i]   = Jnuvp
@@ -71,7 +43,7 @@ def _HM_background_impl(elID,redshift, version, alpha_UV):
         nup = (elID[ekeysA[i]].ip+extra+ediff)*elvolt/planck
         num = (elID[ekeysA[i]].ip+extra-ediff)*elvolt/planck
         Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
+        Jnuvm = np.interp(num,nurev,Jnurev)
         nuadd[2*i+cntr]    = nup
         nuadd[2*i+cntr+1]  = num
         Jnuadd[2*i+cntr]   = Jnuvp
@@ -84,7 +56,7 @@ def _HM_background_impl(elID,redshift, version, alpha_UV):
         nup = (elID[ekeysB[i]].ip+extra+ediff)*elvolt/planck
         num = (elID[ekeysB[i]].ip+extra-ediff)*elvolt/planck
         Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
+        Jnuvm = np.interp(num,nurev,Jnurev)
         nuadd[2*i+cntr]    = nup
         nuadd[2*i+cntr+1]  = num
         Jnuadd[2*i+cntr]   = Jnuvp
@@ -98,8 +70,34 @@ def _HM_background_impl(elID,redshift, version, alpha_UV):
     nut  =  nut[argsrt]
     egyt = nut * planck # energies
 
+    return nut, Jnut, egyt
+
+def HMbackground(elID,redshift=3.0, version='12', alpha_UV=0):
+    Jnu, nu, discz = _HM_background_impl(elID, redshift, version, alpha_UV)
+    logger.log("info", "Using HM{1:s} background at z={0:f}".format(discz, version))
+    return Jnu, nu
+
+def _HM_background_impl(elID,redshift, version, alpha_UV):
+    if version in {'12', '15'}:
+        usecols = tuple(range(60))
+    elif version == '05':
+        usecols = tuple(range(50))
+    data = np.loadtxt(os.path.join(os.path.dirname(__file__), "data/radfields/HM{:s}_UVB.dat").format(version), usecols=usecols)
+    rdshlist = data[0,:]
+    amin = np.argmin(np.abs(rdshlist-redshift))
+    waveAt, Jnut = data[1:,0], data[1:,amin+1]
+    waveA = waveAt[1:]*1.0E-10
+    #w = np.where(waveAt < 912.0)
+    Jnu = Jnut[1:]
+    nu = 299792458.0/waveA
+    # Interpolate the Haardt & Madau background around each of the ionization potentials
+    
+    nut, Jnut, egyt = extra_interp(elID, nu, Jnu)
+
     # Shape parameter (Crighton et al 2015, https://arxiv.org/pdf/1406.4239.pdf)
     if alpha_UV != 0:
+        const = constants.get()
+        elvolt  = const["elvolt"]
         logJ = np.log10(Jnut)
         e0  = elID["H I"].ip * elvolt
         e1  = 10 * elID["H I"].ip * elvolt
@@ -118,77 +116,41 @@ def _HM_background_impl(elID,redshift, version, alpha_UV):
 
 def HM_fiducial(elID,redshift, version):
     return _HM_background_impl(elID, redshift, version, 0.0)[0]
-    #return np.loadtxt(os.path.join(os.path.dirname(__file__), "data/radfields/fiducial_j0_hm{:s}.dat").format(version), usecols=1)
 
-def powerlaw(elID,):
-    const = constants.get()
-    planck  = const["planck"]
-    elvolt  = const["elvolt"]
+def AGN(elID):
+    nu, Jnu = np.loadtxt(os.path.join(os.path.dirname(__file__), "data/radfields/agn_n4.radfield"), usecols=(0,1), unpack=True)
+    Jnu /= nu # nu J_nu is output by Cloudy
+
+    # Now load the HM spectrum to get the same frequency scale
+    HMdata = np.loadtxt(os.path.join(os.path.dirname(__file__), "data/radfields/HM12_UVB.dat"), usecols=tuple(range(60)))
+    rdshlist = HMdata[0,:]
+    amin = np.argmin(np.abs(rdshlist-0.0))
+    waveAt, Jnut = HMdata[1:,0], HMdata[1:,amin]
+    waveA = waveAt[1:]*1.0E-10
+    nu_HM = 299792458.0/waveA
+    Jnu = np.interp(nu_HM, nu, Jnu)
+    # Extra interpolation for IPs
+    nu, Jnu, _ = extra_interp(elID, nu_HM, Jnu)
+    
+    return Jnu, nu
+
+def powerlaw(elID):
     try:
-        nurevt, Jnurevt = np.loadtxt("data/radfields/" + options["radfield"]+".radfield",unpack=True)
+        nut, Jnut = np.loadtxt("data/radfields/" + options["radfield"]+".radfield",unpack=True)
         # Now load the HM spectrum to get the same frequency scale
         data = np.loadtxt("HM12_UVB.dat")
         rdshlist = data[0,:]
         amin = np.argmin(np.abs(rdshlist-3.0))
         waveAt, Jnut = data[1:,0], data[1:,amin]
         waveA = waveAt[1:]*1.0E-10
-        nu = 299792458.0/waveA
-        # Interpolate the background around each of the ionization potentials
-        ediff = 1.0E-10
-        ekeys = elID.keys()
-        nurev  = nu[::-1]
-        Jnurev = np.interp(nurev, nurevt, Jnurevt)
+        nu_HM = 299792458.0/waveA
+        Jnut = np.interp(nu_HMrev, nut, Jnut)
     except:
         logger.log("critical", "Radiation field file: {0:s} does not exist".format(options["radfield"]+".radfield"))
         sys.exit()
-    ediff = 1.0E-10
-    ekeys = elID.keys()
-    Jnuadd = np.zeros(2*len(ekeys) + 2*7) # 7 additional points for secondary heat/ionizations
-    nuadd  = np.zeros(2*len(ekeys) + 2*7) # 7 additional points for secondary heat/ionizations
-    for i in range(len(ekeys)):
-        nup = (elID[ekeys[i]].ip+ediff)*elvolt/planck
-        num = (elID[ekeys[i]].ip-ediff)*elvolt/planck
-        Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
-        nuadd[2*i]    = nup
-        nuadd[2*i+1]  = num
-        Jnuadd[2*i]   = Jnuvp
-        Jnuadd[2*i+1] = Jnuvm
-    # Now include the additional points for secondary heat/ionizations
-    ekeysA = ["H I", "D I", "He I", "He II"]
-#	ekeysA = ["H I", "He I", "He II"]
-    extra = 28.0
-    cntr = 2*len(ekeys)
-    for i in range(len(ekeysA)):
-        nup = (elID[ekeysA[i]].ip+extra+ediff)*elvolt/planck
-        num = (elID[ekeysA[i]].ip+extra-ediff)*elvolt/planck
-        Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
-        nuadd[2*i+cntr]    = nup
-        nuadd[2*i+cntr+1]  = num
-        Jnuadd[2*i+cntr]   = Jnuvp
-        Jnuadd[2*i+cntr+1] = Jnuvm
-    ekeysB = ["H I", "D I", "He I"]
-#	ekeysB = ["H I", "He I"]
-    extra = 11.0
-    cntr = 2*len(ekeys) + 2*4
-    for i in range(len(ekeysB)):
-        nup = (elID[ekeysB[i]].ip+extra+ediff)*elvolt/planck
-        num = (elID[ekeysB[i]].ip+extra-ediff)*elvolt/planck
-        Jnuvp = np.interp(nup,nurev,Jnurev)
-        Jnuvm = np.interp(nup,nurev,Jnurev)
-        nuadd[2*i+cntr]    = nup
-        nuadd[2*i+cntr+1]  = num
-        Jnuadd[2*i+cntr]   = Jnuvp
-        Jnuadd[2*i+cntr+1] = Jnuvm
-    # Append to the original arrays
-    Jnut = np.append(Jnurev,Jnuadd)
-    nut  = np.append(nurev,nuadd)
-    argsrt = np.argsort(nut,kind='mergesort')
-    #plt.plot(np.log10(nu[::-1]),np.log10(Jnu[::-1]),'bo')
-    #plt.plot(np.log10(nut[argsrt]),np.log10(Jnut[argsrt]),'rx')
-    #plt.show()
-    return Jnut[argsrt], nut[argsrt]
+
+    Jnut, nut, _ = extra_interp(elID, nut, Jnut)
+    return Jnut, nut
 
 def test_background(elID):
     const = constants.get()
@@ -245,38 +207,3 @@ def test_background(elID):
     #plt.plot(np.log10(nut[argsrt]),np.log10(Jnut[argsrt]),'rx')
     #plt.show()
     return Jnut[argsrt], nut[argsrt]
-
-#def powerlaw(nuzero, index=-1.0, ioniznparam=-2.0, options=None, hionpot=13.598434005136):
-#	"""
-#	Important note: What is returned from this function needs to be
-#	multiplied by the total H volume density before it is added to the
-#	HM12 background flux
-#	"""
-#	if options is None: options = getoptions.default()
-#	# First, define the shape of the radiation field
-#	planck  = options["const"]["planck"]
-#	elvolt  = options["const"]["elvolt"]
-#	loRyd = 9.115E-3
-#	hiRyd = 3676.0
-#	nuRyd = nuzero*planck/(hionpot*elvolt)
-#	lonu = loRyd * hionpot*elvolt/planck
-#	hinu = hiRyd * hionpot*elvolt/planck
-#	flux = (nuzero)**index
-#	lojoin = np.argmin(np.abs(nuRyd-loRyd))
-#	fxlo = flux[lojoin]
-#	hijoin = np.argmin(np.abs(nuRyd-hiRyd))
-#	fxhi = flux[hijoin]
-#	wlo = np.where(nuRyd<loRyd)
-#	whi = np.where(nuRyd>hiRyd)
-#	flux[wlo] = fxlo * (nuzero[wlo]**2.5) / (lonu**2.5)
-#	flux[whi] = fxhi * (nuzero[whi]**-2.0) / (hinu**-2.0)
-#	plt.plot(np.log10(nuzero),np.log10(flux),'k-')
-#	mn = np.min(np.log10(flux))
-#	mx = np.max(np.log10(flux))
-#	#plt.plot([np.log10(lonu),np.log10(lonu)],[mn,mx],'r-')
-#	#plt.plot([np.log10(hinu),np.log10(hinu)],[mn,mx],'r-')
-#	#plt.show()
-#	# Now normalize to the appropriate number of H ionizing photons
-#	nioniz = calc_Jnur.nhydroion(flux, nuzero, planck, hionpot*elvolt/planck)
-#	fact = 2.99792458E10 * (10.0**ioniznparam) / (4.0*np.pi*nioniz)
-#	return fact*flux
